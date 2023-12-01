5a8cb458192fdb81a3766de81c2a05271f2a0921
fullmoon03 <borem03@gmail.com>
2023-12-01 17:09:38 +0900

    fix: allowdhost

--- bsk_project/bsk_project/settings.py
+++ bsk_project/bsk_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-65y4&&=lm@wbi$igz_s3$7)i^dru9^_x3s#la0xph^x)_&mxo(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
