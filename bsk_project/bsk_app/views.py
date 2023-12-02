7477b183b308af84cf89deee9502057fb6cb92fb
fullmoon03 <borem03@gmail.com>
2023-12-02 10:18:19 +0900

    프로필 수정 양식 변경

--- bsk_project/bsk_app/views.py
+++ bsk_project/bsk_app/views.py
@@ -124,7 +124,14 @@ class AuthView(APIView):
             serializer.save()
         else:
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        # return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(  # 로그인과 같은 양식으로 데이터 반환(태균 요청)
+        {
+            "user": serializer.data,
+            "message": "login success",
+        },
+        status=status.HTTP_200_OK,
+    )
 
     @authentication_classes([JWTAuthentication])
     @permission_classes([IsAuthenticated])
